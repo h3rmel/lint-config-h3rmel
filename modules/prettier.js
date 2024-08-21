4da06dae56a56878f3d2246ff38e917b47f67482
H3rmel <isaachermel@gmail.com>
2024-08-21 14:31:13 -0300

    module.exports

--- modules/prettier.js
+++ modules/prettier.js
@@ -2,7 +2,7 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-export default {
+module.exports = {
   printWidth: 120,
   tabWidth: 2,
   singleQuote: true,
@@ -13,5 +13,5 @@ export default {
   endOfLine: "auto",
   bracketSpacing: true,
   bracketSameLine: false,
-  plugins: ['prettier-plugin-tailwindcss']
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
